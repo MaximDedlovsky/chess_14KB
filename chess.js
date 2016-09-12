c6e87df3abd7ae79406f0fbc305b08fcc2c3c864
Дмитрий Баранов <demon.asgard@gmail.com>
2016-09-12 09:24:27 +0500

    Вывод пешек

--- chess.js
+++ chess.js
@@ -3,11 +3,31 @@
  */
 
 $(function() {
+    var figures = [{
+        row: 1,
+        column: 0,
+        color: "black",
+        type: "pawn",
+        text: "♟"
+    }, {
+        row: 1,
+        column: 1,
+        color: "black",
+        type: "pawn",
+        text: "♟"
+    }];
+
     var board = $("<table>");
     for(var i = 0; i < 8; i++) {
         var tr = $("<tr>");
         for(var j = 0; j < 8; j++) {
             var td = $("<td>");
+            var f = _.find(figures, function(f) {
+                return f.row == i && f.column == j;
+            });
+            if(f) {
+                td.text(f.text);
+            }
             tr.append(td);
         }
         board.append(tr);
