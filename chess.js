fc26e061fff254f75f56bd8b9f6ba14fb4bfc939
Дмитрий Баранов <demon.asgard@gmail.com>
2016-09-12 09:08:08 +0500

    Вывод доски

--- chess.js
+++ chess.js
@@ -1,3 +1,16 @@
 /**
  * Created by demon on 12.09.16.
  */
+
+$(function() {
+    var board = $("<table>");
+    for(var i = 0; i < 8; i++) {
+        var tr = $("<tr>");
+        for(var j = 0; j < 8; j++) {
+            var td = $("<td>");
+            tr.append(td);
+        }
+        board.append(tr);
+    }
+    $("#board").html(board);
+});
